318006738a384d321478b65e7f37cd63da1d6515
Shalin <shalinmehta12@gmail.com>
2026-08-22 21:14:56 +0000

    import .tsx fix

--- position-tracker/app/page.tsx
+++ position-tracker/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-import Footer from "./components/footer.tsx";
+import Footer from "./components/footer";
 
 export default function Home() {
   return (
